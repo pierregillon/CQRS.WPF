0042c272369723b854782ecdd212fd2b45804152
Pierre GILLON <gillon.pierre69@gmail.com>
2015-09-22 10:31:27 +0200

    Refactor the CustomerListViewModel tests and remove non necessary tests

--- sources/Client.Wpf.Business.Tests/CustomerListViewModelShould.cs
+++ sources/Client.Wpf.Business.Tests/CustomerListViewModelShould.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -16,26 +17,7 @@ namespace Client.Wpf.Business.Tests
 {
     public class CustomerListViewModel_should
     {
-        private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
-        private CustomerListViewModel _customerListViewModel;
-        private readonly IEnumerable<CustomerListItem> SOME_CUSTOMERS = new List<CustomerListItem>
-        {
-            new CustomerListItem(),
-            new CustomerListItem(),
-        };
-
-        private readonly IEnumerable<CustomerListItem> SOME_OTHER_CUSTOMERS = new[]
-        {
-            new CustomerListItem(),
-            new CustomerListItem(),
-            new CustomerListItem(),
-            new CustomerListItem(),
-        };
-        private readonly CustomerListItem A_CUSTOMER = new CustomerListItem{Id = Guid.NewGuid()};
-
-        private readonly Mock<ICustomerListService> _customerListServiceMock;
-        private readonly Mock<IDialogService> _dialogServiceMock;
-        private readonly Mock<IMessenger> _messengerServiceMock;
+        // ----- Initialization
 
         public CustomerListViewModel_should()
         {
@@ -44,86 +26,16 @@ namespace Client.Wpf.Business.Tests
             _dialogServiceMock = new Mock<IDialogService>();
 
             _customerListViewModel = new CustomerListViewModel(_messengerServiceMock.Object, _customerListServiceMock.Object, _dialogServiceMock.Object);
-            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(SOME_CUSTOMERS);
+            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(INITIAL_CUSTOMERS);
             _customerListViewModel.Boot().Wait();
         }
 
-        [Fact]
-        public void load_customers_from_the_customer_list_service_when_booted()
-        {
-            Check.That(_customerListViewModel.Customers).Contains(SOME_CUSTOMERS);
-        }
-
-        [Fact]
-        public void end_loading_when_boot_is_completed()
-        {
-            // Arrange
-            _customerListViewModel = new CustomerListViewModel(_messengerServiceMock.Object, _customerListServiceMock.Object, _dialogServiceMock.Object);
-
-            // Acts
-            _customerListViewModel.Boot().Wait();
-
-            // Asserts
-            Check.That(_customerListViewModel.IsLoading).IsFalse();
-        }
-
-        [Fact]
-        public void have_no_selected_customer_when_boot_is_completed()
-        {
-            Check.That(_customerListViewModel.SelectedCustomer).IsNull();
-        }
-
-        [Fact]
-        public void send_a_message_to_display_customer_details_when_opening_customer_details()
-        {
-            // Arrange
-            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(SOME_CUSTOMERS.Union(new[] {A_CUSTOMER}));
-            var expectedMessage = new DisplayCustomerDetailsMessage(A_CUSTOMER.Id);
-
-            // Acts
-            _customerListViewModel.Boot().Wait();
-            _customerListViewModel.DisplayCustomerDetailsCommand.Execute(A_CUSTOMER);
-
-            // Asserts
-            _messengerServiceMock.Verify(x => x.Send(expectedMessage), Times.Once);
-        }
+        // ----- Tests
 
         [Fact]
         public void be_a_unclosable_tab()
         {
             Check.That(_customerListViewModel.CanClose).IsFalse();
-        }
-
-        [Fact]
-        public void update_the_displayed_customers_when_refresh_invoked()
-        {
-            // Arrange
-            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(SOME_OTHER_CUSTOMERS);
-
-            // Acts
-            _customerListViewModel.RefreshCommand.ExecuteAsync().Wait();
-
-            // Asserts
-            Check.That(_customerListViewModel.Customers).Contains(SOME_OTHER_CUSTOMERS);
-        }
-
-        [Fact]
-        public void load_when_refreshing_the_displayed_customers()
-        {
-            // Arrange
-            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(() =>
-            {
-                Wait();
-                return SOME_OTHER_CUSTOMERS;
-            });
-
-            // Acts
-            var refreshTask =_customerListViewModel.RefreshCommand.ExecuteAsync();
-            
-            // Asserts
-            Check.That(_customerListViewModel.IsLoading).IsTrue();
-            EndLoad(refreshTask);
-            Check.That(_customerListViewModel.IsLoading).IsFalse();
         }
 
         [Fact]
@@ -145,6 +57,65 @@ namespace Client.Wpf.Business.Tests
             Check.That(_customerListViewModel.IsLoading).IsFalse();
         }
 
+        [Fact]
+        public void displays_customers_from_the_customer_list_service_when_booted()
+        {
+            Check.That(_customerListViewModel.Customers).Contains(INITIAL_CUSTOMERS);
+        }
+
+        [Fact]
+        public void have_no_selected_customer_when_booted()
+        {
+            Check.That(_customerListViewModel.SelectedCustomer).IsNull();
+        }
+
+        [Fact]
+        public void send_a_message_to_display_customer_details_when_opening_customer_details()
+        {
+            // Arrange
+            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(SOME_CUSTOMERS.Union(new[] {A_CUSTOMER}));
+            var expectedMessage = new DisplayCustomerDetailsMessage(A_CUSTOMER.Id);
+
+            // Acts
+            _customerListViewModel.Boot().Wait();
+            _customerListViewModel.DisplayCustomerDetailsCommand.Execute(A_CUSTOMER);
+
+            // Asserts
+            _messengerServiceMock.Verify(x => x.Send(expectedMessage), Times.Once);
+        }
+
+        [Fact]
+        public void be_loading_when_refreshing_the_displayed_customers()
+        {
+            // Arrange
+            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(() =>
+            {
+                Wait();
+                return SOME_OTHER_CUSTOMERS;
+            });
+
+            // Acts
+            var refreshTask =_customerListViewModel.RefreshCommand.ExecuteAsync();
+            
+            // Asserts
+            Check.That(_customerListViewModel.IsLoading).IsTrue();
+            EndLoad(refreshTask);
+            Check.That(_customerListViewModel.IsLoading).IsFalse();
+        }
+        
+        [Fact]
+        public void update_the_displayed_customers_when_refresh_invoked()
+        {
+            // Arrange
+            _customerListServiceMock.Setup(x => x.GetCustomers()).Returns(SOME_OTHER_CUSTOMERS);
+
+            // Acts
+            _customerListViewModel.RefreshCommand.ExecuteAsync().Wait();
+
+            // Asserts
+            Check.That(_customerListViewModel.Customers).Contains(SOME_OTHER_CUSTOMERS);
+        }
+
         // ----- Private methods
         private void Wait()
         {
@@ -155,5 +126,33 @@ namespace Client.Wpf.Business.Tests
             _manualResetEvent.Set();
             bootTask.Wait();
         }
+
+        // ----- Fields
+        private readonly CustomerListViewModel _customerListViewModel;
+        private readonly Mock<ICustomerListService> _customerListServiceMock;
+        private readonly Mock<IDialogService> _dialogServiceMock;
+        private readonly Mock<IMessenger> _messengerServiceMock;
+
+        // ----- Data
+        private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+        private readonly IEnumerable<CustomerListItem> SOME_CUSTOMERS = new List<CustomerListItem>
+        {
+            new CustomerListItem(),
+            new CustomerListItem(),
+            new CustomerListItem(),
+        };
+        private readonly IEnumerable<CustomerListItem> SOME_OTHER_CUSTOMERS = new[]
+        {
+            new CustomerListItem(),
+            new CustomerListItem(),
+            new CustomerListItem(),
+            new CustomerListItem(),
+        };
+        private readonly IEnumerable<CustomerListItem> INITIAL_CUSTOMERS = new[]
+        {
+            new CustomerListItem(),
+            new CustomerListItem(),
+        };
+        private readonly CustomerListItem A_CUSTOMER = new CustomerListItem { Id = Guid.NewGuid() };
     }
 }
